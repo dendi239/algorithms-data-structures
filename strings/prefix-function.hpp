fd643cb69a2b6bb49989f2520e3b1c5b5c4fdeaf
Denys Smirnov <dendi239@gmail.com>
2020-02-24 23:11:42 +0200

    update misstype, refactor code

--- strings/prefix-function.hpp
+++ strings/prefix-function.hpp
@@ -1,17 +1,18 @@
 #include <string>
 #include <vector>
 
-class PrefixFunciton {
+class PrefixFunction {
  public:
-  explicit PrefixFunciton(const std::string &str)
+  explicit PrefixFunction(const std::string &str)
   : text(str), values(str.size() + 1) {
-    for (size_t i = 1; i < str.size(); ++i) {
-      if (str[0] == str[1]) values[i + 1] = 1;
-      for (size_t j = i, lenght; lenght = values[j], j > 0; j = values[j]) {
-        if (str[i] == str[lenght]) {
-          values[i + 1] = lenght + 1;
+    for (size_t index = 1; index < str.size(); ++index) {
+      for (size_t start = index, length; length = values[start], 
+           start > 0; start = values[start]) {
+        if (text[index] == text[length]) {
+          values[index + 1] = length + 1;
           break;
         }
+        
       }
     }
   }
