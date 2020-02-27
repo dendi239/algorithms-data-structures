eedc90a1a80d98baf74e8e6bb833b9720036f931
Denys Smirnov <dendi239@gmail.com>
2020-02-28 00:09:39 +0200

    update indents, add necessary cast

--- strings/z-function.hpp
+++ strings/z-function.hpp
@@ -2,30 +2,29 @@
 #include <vector>
 
 auto z_function(const std::string &text) -> std::vector<int> {
-    std::vector<int> values(text.size());
-    int start = 0, end = 0;
+  std::vector<int> values(text.size());
+  int start = 0, end = 0;
 
-    for (int index = 1; index < text.size(); ++index) {
-        int finish = index;
+  for (int index = 1; index < static_cast<int>(text.size()); ++index) {
+    int finish = index;
 
-        if (end > index) {
-            finish = index + values[index - start];
-            finish = std::min(finish, end);
-        }
-
-        for (; finish < text.size(); ++finish) {
-            if (text[finish] != text[finish - index]) {
-                break;
-            }
-        }
-
-        if (end < finish) {
-            start = index, end = finish;
-        }
-
-        values[index] = finish - index;
+    if (end > index) {
+      finish = index + values[index - start];
+      finish = std::min(finish, end);
     }
 
+    for (; finish < text.size(); ++finish) {
+      if (text[finish] != text[finish - index]) {
+        break;
+      }
+    }
 
-    return values;
+    if (end < finish) {
+      start = index, end = finish;
+    }
+
+    values[index] = finish - index;
+  }
+
+  return values;
 }
