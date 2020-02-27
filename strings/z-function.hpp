38822e066c552f33ef98422785a8162c7a2f4de1
Denys Smirnov <dendi239@gmail.com>
2020-02-27 23:18:24 +0200

    add z-function

---
+++ strings/z-function.hpp
@@ -0,0 +1,31 @@
+#include <string>
+#include <vector>
+
+auto z_function(const std::string &text) -> std::vector<int> {
+    std::vector<int> values(text.size());
+    int start = 0, end = 0;
+
+    for (int index = 1; index < text.size(); ++index) {
+        int finish = index;
+
+        if (end > index) {
+            finish = index + values[index - start];
+            finish = std::min(finish, end);
+        }
+
+        for (; finish < text.size(); ++finish) {
+            if (text[finish] != text[finish - index]) {
+                break;
+            }
+        }
+
+        if (end < finish) {
+            start = index, end = finish;
+        }
+
+        values[index] = finish - index;
+    }
+
+
+    return values;
+}
