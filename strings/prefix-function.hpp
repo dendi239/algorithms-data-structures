6cd8aef85b1f3a2b775af01bffe6f6c10cb0daaa
Denys Smirnov <dendi239@gmail.com>
2020-03-05 10:00:37 +0200

    remove useless class, turn it to a function

--- strings/prefix-function.hpp
+++ strings/prefix-function.hpp
@@ -1,27 +1,18 @@
 #include <string>
 #include <vector>
 
-class PrefixFunction {
- public:
-  explicit PrefixFunction(const std::string &str)
-  : text(str), values(str.size() + 1) {
-    for (size_t index = 1; index < str.size(); ++index) {
-      for (size_t start = index, length; length = values[start], 
-           start > 0; start = values[start]) {
-        if (text[index] == text[length]) {
-          values[index + 1] = length + 1;
-          break;
+auto prefix_function(const std::string &text) -> std::vector<size_t> {
+    auto values = std::vector<size_t>(text.size() + 1);
+    
+    for (size_t index = 1; index < text.size(); ++index) {
+        for (size_t start = index, length; length = values[start],
+             start > 0; start = values[start]) {
+            if (text[index] == text[length]) {
+                values[index + 1] = length + 1;
+                break;
+            }
         }
-        
-      }
     }
-  }
-
-  auto operator()(size_t prefix_size) const -> size_t {
-    return values[prefix_size];
-  }
-
- private:
-  std::string text;
-  std::vector<size_t> values;
-};
+    
+    return values;
+}
