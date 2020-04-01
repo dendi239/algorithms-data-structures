0ebe007132efb425696c5cfc1abe7a379cff846e
Denys Smirnov <dendi239@gmail.com>
2020-04-01 20:20:20 +0300

    Make prefix function work with any string-like structure, not only basic string.

--- strings/prefix-function.hpp
+++ strings/prefix-function.hpp
@@ -1,8 +1,9 @@
 #include <string>
 #include <vector>
 
-auto prefix_function(const std::string &text) -> std::vector<size_t> {
-    auto values = std::vector<size_t>(text.size() + 1);
+template <class String>
+auto prefix_function(const String &text) -> std::vector<int> {
+    auto values = std::vector<int>(text.size() + 1);
     
     for (size_t index = 1; index < text.size(); ++index) {
         for (size_t start = index, length; length = values[start],
