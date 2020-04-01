07f89f24c14356eab261e7e36072623ec0f4ee6b
Denys Smirnov <dendi239@gmail.com>
2020-04-01 20:20:50 +0300

    Reorder contents

--- ranges/iterator_range.hpp
+++ ranges/iterator_range.hpp
@@ -1,26 +1,18 @@
 #pragma once
 
+#include <iterator>
+#include <utility>
+
 template <class Iterator>
 class IteratorRange {
-public:
-    IteratorRange(Iterator begin, Iterator end) : begin_{std::move(begin)}, end_{std::move(end)} {
-        // no-op
-    }
+ public:
+  IteratorRange(Iterator begin, Iterator end)
+      : begin_{std::move(begin)}, end_{std::move(end)}
+  {}
 
-    template <class Container>
-    explicit IteratorRange(const Container &container)
-    : begin_{std::begin(container)}, end_(std::end(container)) {
-        // no-op
-    }
+  auto begin() const { return begin_; }
+  auto end() const { return end_; }
 
-    auto begin() const {
-        return begin_;
-    }
-
-    auto end() const {
-        return end_;
-    }
-
-private:
-    Iterator begin_, end_;
+ private:
+  Iterator begin_, end_;
 };
