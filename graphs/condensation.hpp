9fcc74d99c94d9af0438fc9f58e9ba2e0bb7f5e5
Denys Smirnov <dendi239@gmail.com>
2020-05-01 00:05:09 +0300

    Update Condensation algorithm to work with any graph

--- graphs/condensation.hpp
+++ graphs/condensation.hpp
@@ -1,27 +1,42 @@
 #pragma once
 
+#include <unordered_map>
+#include <unordered_set>
 #include <vector>
 
 #include <data-structures/partition.hpp>
+#include <graphs/visitor.hpp>
 #include <graphs/graph.hpp>
+
+namespace condensation {
+
+using Graph = std::vector<std::vector<int>>;
 
 struct Condensation {
   Graph graph;
   Partition components;
 };
 
+template<class Graph>
 auto BuildCondensation(const Graph &graph) -> Condensation;
 
+namespace detail {
+
 auto ReversedGraph(const Graph &graph) -> Graph;
+
+template<class Graph>
 auto DfsExitOrder(const Graph &graph) -> std::vector<int>;
 
-auto BuildCondensation(const Graph &graph) -> Condensation {
-  auto order = DfsExitOrder(graph);
-  auto reversed_graph = ReversedGraph(graph);
+}  // namespace detail
 
-  auto partition_builder = PartitionBuilder(graph.Size());
-  LambdaVisitor()
-      .OnStart([&](int){ partition_builder.CreateNewGroup(); })
+template<class Graph>
+auto BuildCondensation(const Graph &graph) -> Condensation {
+  auto order = detail::DfsExitOrder(graph);
+  auto reversed_graph = detail::ReversedGraph(graph);
+
+  auto partition_builder = PartitionBuilder(graph.size());
+  traverses::LambdaVisitor()
+      .OnStart([&](int) { partition_builder.CreateNewGroup(); })
       .OnFinish([&](int node) { partition_builder.AddElementToLastGroup(node); })
       .Visit(reversed_graph, order);
 
@@ -30,9 +45,9 @@ auto BuildCondensation(const Graph &graph) -> Condensation {
   size_t condensation_size = partition.GroupsNumber();
   auto unique_condensation_links = std::vector<std::unordered_set<size_t>>(condensation_size);
 
-  for (size_t node = 0; node < graph.Size(); ++node) {
+  for (size_t node = 0; node < graph.size(); ++node) {
     size_t from = partition.GroupId(node);
-    for (size_t link : graph.Links(node)) {
+    for (size_t link : OutgoingEdges(graph, node)) {
       size_t to = partition.GroupId(link);
       if (from != to) {
         unique_condensation_links[from].insert(to);
@@ -43,16 +58,19 @@ auto BuildCondensation(const Graph &graph) -> Condensation {
   auto condensation = Graph(condensation_size);
   for (size_t node = 0; node < condensation_size; ++node) {
     for (size_t link : unique_condensation_links[node]) {
-      condensation.Connect(node, link);
+      Connect(&condensation, node, link);
     }
   }
 
   return {condensation, partition};
 }
 
+namespace detail {
+
+template <class Graph>
 auto DfsExitOrder(const Graph &graph) -> std::vector<int> {
   std::vector<int> order;
-  LambdaVisitor()
+  traverses::LambdaVisitor()
       .OnFinish([&order](int node) { order.push_back(node); })
       .Visit(graph);
   std::reverse(order.begin(), order.end());
@@ -60,11 +78,15 @@ auto DfsExitOrder(const Graph &graph) -> std::vector<int> {
 }
 
 auto ReversedGraph(const Graph &graph) -> Graph {
-  auto reversed_graph = Graph(graph.Size());
-  for (size_t node = 0; node < reversed_graph.Size(); ++node) {
-    for (auto link : graph.Links(node)) {
-      reversed_graph.Connect(link, node);
+  auto reversed_graph = Graph(graph.size());
+  for (size_t node = 0; node < reversed_graph.size(); ++node) {
+    for (auto link : OutgoingEdges(graph, node)) {
+      Connect(&reversed_graph, link, node);
     }
   }
   return reversed_graph;
 }
+
+}  // namespace detail
+
+}  // namespace condensation
