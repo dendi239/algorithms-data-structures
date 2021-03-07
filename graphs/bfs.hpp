7cd9e88848b1ae2e52ab76ce7e886e60db01f57f
Denys Smirnov <dendi239@gmail.com>
2021-03-07 19:25:21 +0200

    Update indents

--- graphs/bfs.hpp
+++ graphs/bfs.hpp
@@ -7,43 +7,45 @@ namespace traverses {
 
 template <class Vertex, class Edge>
 class BfsVisitor {
-public:
-    virtual void DiscoverVertex(Vertex /*vertex*/) {}
-    virtual void ExamineEdge(const Edge & /*edge*/) {}
-    virtual void ExamineVertex(Vertex /*vertex*/) {}
-    virtual ~BfsVisitor() = default;
+ public:
+  virtual void DiscoverVertex(Vertex /*vertex*/) {}
+  virtual void ExamineEdge(const Edge & /*edge*/) {}
+  virtual void ExamineVertex(Vertex /*vertex*/) {}
+  virtual ~BfsVisitor() = default;
 };
 
 template <class Vertex, class Graph, class Visitor>
-void BreadthFirstSearch(Vertex node, const Graph &graph, Visitor visitor);
+void BreadthFirstSearch(Vertex origin_vertex, const Graph &graph,
+                        Visitor visitor);
 
 }  // namespace traverses
 
 namespace traverses {
 
-template <class NodeType, class Graph, class Visitor>
-void BreadthFirstSearch(NodeType node, const Graph &graph, Visitor visitor) {
-    std::unordered_set<NodeType> discovered_nodes;
-    std::queue<NodeType> queue;
+template <class Vertex, class Graph, class Visitor>
+void BreadthFirstSearch(Vertex origin_vertex, const Graph &graph,
+                        Visitor visitor) {
+  std::unordered_set<Vertex> discovered_nodes;
+  std::queue<Vertex> queue;
 
-    auto discover = [&discovered_nodes, &queue, &visitor](NodeType node) {
-        if (discovered_nodes.count(node)) return;
-        discovered_nodes.insert(node);
-        queue.push(node);
-        visitor.DiscoverVertex(node);
-    };
+  auto discover = [&discovered_nodes, &queue, &visitor](Vertex node) {
+    if (discovered_nodes.count(node)) return;
+    discovered_nodes.insert(node);
+    queue.push(node);
+    visitor.DiscoverVertex(node);
+  };
 
-    for (discover(node); !queue.empty(); queue.pop()) {
-        auto head = queue.front();
-        visitor.ExamineVertex(head);
+  for (discover(origin_vertex); !queue.empty(); queue.pop()) {
+    auto head = queue.front();
+    visitor.ExamineVertex(head);
 
-        for (const auto &edge : OutgoingEdges(graph, head)) {
-            visitor.ExamineEdge(edge);
+    for (const auto &edge : OutgoingEdges(graph, head)) {
+      visitor.ExamineEdge(edge);
 
-            auto target = GetTarget(graph, edge);
-            discover(target);
-        }
+      auto target = GetTarget(graph, edge);
+      discover(target);
     }
+  }
 }
 
 }  // namespace traverses
